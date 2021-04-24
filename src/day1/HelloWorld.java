1712234883c3d3d0e198a4b1e094325ea30315d9
halideimir <halideimir@gmail.com>
2021-04-24 16:57:44 -0400

    last step

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("today is end");
         System.out.println("we did it");
+        System.out.println("good job");
     }
 }
